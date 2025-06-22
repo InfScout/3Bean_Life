f818c570fa8ac5d439e1b977514ba48633ff5590
Luke Sarun Tighe <DOGEisLIFE2018@gmail.com>
2025-06-22 17:22:45 -0600

    Added Animations
    including idle walking animations

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -6,10 +6,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float baseSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 movement;
-
+    private Animator animator;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -19,6 +21,16 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
+        animator.SetBool("isWalking", true);
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("lastInputX", movement.x);
+            animator.SetFloat("lastInputY", movement.y);
+        }
+        
         movement = context.ReadValue<Vector2>();
+        animator.SetFloat("CurrentX", movement.x);
+        animator.SetFloat("CurrentY", movement.y);
     }
 }
