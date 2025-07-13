9f15bd34e39208787df07ec227cc39c5d71a629c
Luke Sarun Tighe <DOGEisLIFE2018@gmail.com>
2025-07-13 13:35:36 -0600

    reverting everything break

--- Assets/Scripts/MapTransition.cs
+++ Assets/Scripts/MapTransition.cs
@@ -1,21 +1,46 @@
+using System;
 using Unity.Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MapTransition : MonoBehaviour
 {
    [SerializeField] PolygonCollider2D mapBoundary;
-   CinemachineConfiner2D confiner;
-
+   CinemachineConfiner2D _confiner;
+   /*[SerializeField] Direction direction;
+   enum Direction { Up, Down, Left, Right }*/
+   
    private void Awake()
    {
-      confiner = FindObjectOfType<CinemachineConfiner2D>();
+      _confiner = GetComponent<CinemachineConfiner2D>();
    }
 
    private void OnTriggerEnter2D(Collider2D collision)
    {
       if (collision.gameObject.CompareTag("Player"))
       {
-         confiner.BoundingShape2D = mapBoundary;
+         _confiner.BoundingShape2D = mapBoundary;
       }
-   }
+   } 
+
+   /*private void UpdatePlayerLocation(GameObject player)
+   {
+      Vector3 newPosition = player.transform.position;
+      switch (direction)
+      {
+         case Direction.Up:
+            newPosition.y += 8;
+            break;
+         case Direction.Down:
+            newPosition.y -= 8;
+            break;
+         case Direction.Left:
+            newPosition.x -= 8;  
+            break;   
+         case Direction.Right:
+            newPosition.x += 8;
+            break;   
+      }
+      player.transform.position = newPosition;
+   }*/
 }
